c8762fa1e2bb1920fe2284e9c1fdb3f210a0db80
Kanin Gleb <gleb.kanin@gmail.com>
2025-04-01 19:07:30 +0300

    Root PATH object added

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,15 @@ import {Error404} from './components/pages/Error404.tsx';
 import {S} from './components/pages/_styles.ts';
 
 
+const PATH = {
+  HOME: '/',
+  PAGE1: '/page1',
+  PAGE2: '/page2',
+  PAGE3: '/page3',
+  ERROR: '/page/error',
+  EXCEPTIONS: '*'
+} as const;
+
 function App() {
   return (
     <div>
@@ -13,21 +22,23 @@ function App() {
       <S.Body>
         <S.NavContainer>
           Navigation:
-          <S.NavWrapper> <NavLink to='/page-1'>Page 1</NavLink> </S.NavWrapper>
-          <S.NavWrapper> <NavLink to='/page-2'>Page 2</NavLink> </S.NavWrapper>
-          <S.NavWrapper> <NavLink to='/page-3'>Page 3</NavLink> </S.NavWrapper>
+          <S.NavWrapper> <NavLink to={PATH.PAGE1}>Page 1</NavLink> </S.NavWrapper>
+          <S.NavWrapper> <NavLink to={PATH.PAGE2}>Page 2</NavLink> </S.NavWrapper>
+          <S.NavWrapper> <NavLink to={PATH.PAGE3}>Page 3</NavLink> </S.NavWrapper>
         </S.NavContainer>
         <S.Content>
           Sneakers:
           <Routes>
-            <Route path="/" element={ <Navigate to={"/page-1"}/> }/>
+            <Route path={PATH.HOME} element={ <Navigate to={"/page-1"}/> }/>
 
-            <Route path="/page-1" element={<PageOne/>}/>
-            <Route path="/page-2" element={<PageTwo/>}/>
-            <Route path="/page-3" element={<PageThree/>}/>
+            <Route path={PATH.PAGE1} element={<PageOne/>}/>
+            <Route path={PATH.PAGE2} element={<PageTwo/>}/>
+            <Route path={PATH.PAGE3} element={<PageThree/>}/>
 
-            <Route path="/page/error" element={ <Error404/> }/>
-            <Route path="*" element={ <Navigate to={"/page/error"}/> }/>
+            {/*<Route path={PATH.ERROR} element={ <Error404/> }/>*/}
+            {/*<Route path={PATH.EXCEPTIONS} element={ <Navigate to={PATH.ERROR}/> }/>*/}
+
+            <Route path={PATH.EXCEPTIONS} element={ <Error404/> }/>
           </Routes>
         </S.Content>
       </S.Body>
