f567aa8bc15d5876cb27b04a0089cb82808cfdce
Kanin Gleb <gleb.kanin@gmail.com>
2025-04-07 20:18:02 +0300

    Styles implemented in Model

--- src/components/pages/Model.tsx
+++ src/components/pages/Model.tsx
@@ -1,7 +1,9 @@
 import {useParams} from 'react-router-dom';
+import {NavLink} from 'react-router';
 import {adidasModulesState, AdidasModelType} from './Adidas.tsx';
 import {pumaModelsState, PumaModelType} from './Puma.tsx';
 import {nikeModelsState, NikeModelType} from './Nike.tsx';
+import {M} from './_styles.ts';
 
 
 type CrossModalType = {
@@ -34,7 +36,15 @@ export const Model = () => {
   return (
     <>
       {currentModel ? (
-        <div style={{textAlign: 'center'}}>
+        <M.SneakersContent>
+          <M.NavWrapper>
+            <NavLink to={`/${model}`}>
+              {model ?
+                `< Back to ${model.charAt(0).toUpperCase()}${model.slice(1).toLowerCase()}` :
+                ''
+              }
+            </NavLink>
+          </M.NavWrapper>
           <h2>{currentModel?.model}</h2>
           <h4>{currentModel?.collection}</h4>
           <h3>{currentModel?.price}</h3>
@@ -44,7 +54,7 @@ export const Model = () => {
             alt={currentModel?.model}
             style={{width: '600px', height: 'auto', marginBottom: '220px'}}
           />
-        </div>
+        </M.SneakersContent>
       ) : (
         <h2 style={{textAlign: 'center'}}>The model is not found.</h2>
       )}
