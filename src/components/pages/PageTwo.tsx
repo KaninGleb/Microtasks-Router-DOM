537414640118014dcef82ffd9a12cbaa6a314a69
Kanin Gleb <gleb.kanin@gmail.com>
2025-04-07 16:04:48 +0300

    Components updated

--- src/components/pages/PageTwo.tsx
+++ src/components/pages/PageTwo.tsx
@@ -1,4 +1,5 @@
 
+
 export const PageTwo = () => {
     return (
         <div>
@@ -17,6 +18,7 @@ export const PageTwo = () => {
                 content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum
                 as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions
                 have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
 
                 Where does it come from?
                 Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC,
