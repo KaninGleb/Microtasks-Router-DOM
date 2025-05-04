02fbb08745adf17536c7988a110ab268be3557b8
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-05 00:00:47 +0300

    App - RRD 6.4 syntaxes implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import {Navigate, NavLink, Route, Routes} from 'react-router';
-import {Adidas} from './components/pages/Adidas.tsx';
-import {Puma} from './components/pages/Puma.tsx';
-import {Nike} from './components/pages/Nike.tsx';
-import {Model} from './components/pages/Model.tsx';
-import {Error404} from './components/pages/Error404.tsx';
+import {NavLink, Outlet} from 'react-router-dom';
 import {S} from './components/pages/_styles.ts';
-import {Prices} from './components/pages/Prices.tsx';
 
 
 export const PATH = {
@@ -14,6 +8,7 @@ export const PATH = {
   PAGE2: '/puma',
   PAGE3: '/nike',
   PAGE4: '/prices',
+  PROTECTED: '/protected',
   EXCEPTIONS: '*'
 } as const;
 
@@ -28,25 +23,26 @@ function App() {
           <S.NavWrapper> <NavLink to={PATH.PAGE2}>Puma</NavLink> </S.NavWrapper>
           <S.NavWrapper> <NavLink to={PATH.PAGE3}>Nike</NavLink> </S.NavWrapper>
           <S.NavWrapper> <NavLink to={PATH.PAGE4}>For wholesalers</NavLink> </S.NavWrapper>
+          <S.NavWrapper><NavLink to={PATH.PROTECTED}>Protected Page</NavLink></S.NavWrapper>
         </S.NavContainer>
         <S.Content>
           {/*RRD - 6.4*/}
-          {/*<Outlet/>*/}
+          <Outlet/>
 
 
           {/*Sneakers:*/}
-          <Routes>
-            <Route path={PATH.HOME} element={ <Navigate to={PATH.PAGE1}/> }/>
+          {/*<Routes>*/}
+          {/*  <Route path={PATH.HOME} element={ <Navigate to={PATH.PAGE1}/> }/>*/}
 
-            <Route path={PATH.PAGE1} element={<Adidas/>}/>
-            <Route path={PATH.PAGE2} element={<Puma/>}/>
-            <Route path={PATH.PAGE3} element={<Nike/>}/>
-            <Route path={PATH.PAGE4} element={<Prices/>}/>
+          {/*  <Route path={PATH.PAGE1} element={<Adidas/>}/>*/}
+          {/*  <Route path={PATH.PAGE2} element={<Puma/>}/>*/}
+          {/*  <Route path={PATH.PAGE3} element={<Nike/>}/>*/}
+          {/*  <Route path={PATH.PAGE4} element={<Prices/>}/>*/}
 
-            <Route path={'/:model/:id'} element={<Model/>}/>
+          {/*  <Route path={'/:model/:id'} element={<Model/>}/>*/}
 
-            <Route path={PATH.EXCEPTIONS} element={ <Error404/> }/>
-          </Routes>
+          {/*  <Route path={PATH.EXCEPTIONS} element={ <Error404/> }/>*/}
+          {/*</Routes>*/}
         </S.Content>
       </S.Body>
       <S.Footer>2025</S.Footer>
