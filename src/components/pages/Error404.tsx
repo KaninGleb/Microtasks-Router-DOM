cf47726fafaf1fb00107e76709f4b2909b0e65b2
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-05 00:06:30 +0300

    Error404 styles added

--- src/components/pages/Error404.tsx
+++ src/components/pages/Error404.tsx
@@ -1,8 +1,9 @@
+import s from '../../components/pages/Prices.module.css';
 
 
 export const Error404 = () => {
     return (
-        <div>
+        <div className={s.pricesContainer}>
             Error404
         </div>
     )
