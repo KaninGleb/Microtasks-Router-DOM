15da1fd6f4222e5ded8e992e7db7b4fd35f63b4c
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-13 21:18:23 +0300

    components - index.ts added

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export {Adidas} from './pages/Adidas'
+export {Puma} from './pages/Puma'
+export {Nike} from './pages/Nike'
+export {Prices} from './pages/Prices/Prices'
+export {Model} from './pages/Model'
+export {Login} from './pages/Login/Login'
