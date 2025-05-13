4a331f62a882901d02deda23f172a29beb6e1989
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-13 21:21:52 +0300

    RRD-6,4 - index.ts added

---
+++ src/RRD-6,4/index.ts
@@ -0,0 +1,7 @@
+export {Croses} from './components/Croses'
+
+export {ProtectedPage} from './pages/ProtectedPage'
+
+export {ProtectedRoute} from './routes/ProtectedRoute'
+
+export * from './routes/router'
