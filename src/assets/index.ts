d8278d352cbaffe5ca7edc5ce6e7bb8bc2592c6c
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-13 21:34:09 +0300

    assets - index.ts Adidas models added

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+// Adidas models
+export { default as adidasModel1 } from '../assets/adidas/adidasModel1.webp'
+export { default as adidasModel2 } from '../assets/adidas/adidasModel2.webp'
+export { default as adidasModel3 } from '../assets/adidas/adidasModel3.webp'
