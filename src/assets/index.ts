73d72ff81c5b29cc61770d0a0cbb73a76cb3b6fe
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-13 21:36:50 +0300

    assets - index.ts Nike models added

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,3 +2,9 @@
 export { default as adidasModel1 } from '../assets/adidas/adidasModel1.webp'
 export { default as adidasModel2 } from '../assets/adidas/adidasModel2.webp'
 export { default as adidasModel3 } from '../assets/adidas/adidasModel3.webp'
+
+
+// Nike models
+export { default as nikeModel1 } from '../assets/nike/nikeModel1.jpg'
+export { default as nikeModel2 } from '../assets/nike/nikeModel2.jpg'
+export { default as nikeModel3 } from '../assets/nike/nikeModel3.jpg'
