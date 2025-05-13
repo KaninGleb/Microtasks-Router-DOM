1c93fc303a0c6872b18c4f7d920579c728ab6527
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-13 18:45:07 +0300

    router refactored

--- src/RRD-6,4/routes/router.tsx
+++ src/RRD-6,4/routes/router.tsx
@@ -1,13 +1,14 @@
-import {createBrowserRouter} from 'react-router';
+import {createBrowserRouter, RouteObject} from 'react-router';
 import App from '../../App.tsx';
-import {Error404} from '../../components/pages/Error404.tsx';
 import {Adidas} from '../../components/pages/Adidas.tsx';
 import {Puma} from '../../components/pages/Puma.tsx';
 import {Nike} from '../../components/pages/Nike.tsx';
 import {Prices} from '../../components/pages/Prices.tsx';
 import {Model} from '../../components/pages/Model.tsx';
-import {ProtectedRoute} from './ProtectedRoute.tsx';
 import {ProtectedPage} from '../pages/ProtectedPage.tsx';
+import {Navigate, Outlet} from 'react-router-dom';
+import {Login} from '../../components/pages/Login/Login.tsx';
+import s from '../../components/pages/Prices.module.css';
 
 
 const PATH = {
@@ -18,29 +19,45 @@ const PATH = {
   MODEL: '/:model/:id',
   PROTECTED: '/protected',
   ERROR: '/error',
+  LOGIN: '/login',
 } as const;
+
+const publicRoutes: RouteObject[] = [
+  { path: PATH.ADIDAS, element: <Adidas/> },
+  { path: PATH.PUMA, element: <Puma/> },
+  { path: PATH.NIKE, element: <Nike/> },
+  { path: PATH.PRICES, element: <Prices/> },
+  { path: PATH.MODEL, element: <Model/> },
+  { path: PATH.ERROR, element: <Login/> },
+]
+
+const privateRoutes: RouteObject[] = [
+  { path: PATH.PROTECTED, element: <ProtectedPage/> },
+]
+
+export const PrivateRoutes = () => {
+  const isAuth = false
+
+  return (
+    <div className={s.pricesContainer}>
+      {isAuth ? <Outlet/> : <Navigate to={PATH.LOGIN}/>}
+    </div>
+  )
+}
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App/>,
-    errorElement: <Error404/>,
+    errorElement: <Navigate to={PATH.ERROR} />,
     children: [
-      {path: PATH.ADIDAS, element: (<Adidas/>)},
-      {path: PATH.PUMA, element: (<Puma/>)},
-      {path: PATH.NIKE, element: (<Nike/>)},
-      {path: PATH.PRICES, element: (<Prices/>)},
-      {path: PATH.MODEL, element: (<Model/>)},
       {
-        path: PATH.PROTECTED, element: (
-          <ProtectedRoute>
-            <ProtectedPage/>
-          </ProtectedRoute>
-        )
+        element: <PrivateRoutes/>,
+        children: privateRoutes
       },
-      {path: PATH.ERROR, element: (<Error404/>)},
+      ...publicRoutes,
     ],
   },
-]);
+])
 
 // useNavigation, useMatches, useRouteError
