f5ab9d093de2dd0f9111b6748e1e47dd9a5ed0b3
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-13 22:51:54 +0300

    Links paths refactored

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import {RouterProvider} from 'react-router-dom';
-import {router} from './RRD-6,4/routes/router.tsx';
+import {router} from './RRD-6,4';
+import './index.css';
 
 
 const root = ReactDOM.createRoot(
