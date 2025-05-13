f018383b2ae45e05054a2125a3befbe66d5c3d3e
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-13 21:39:16 +0300

    assets - index.ts Puma models added

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -8,3 +8,9 @@ export { default as adidasModel3 } from '../assets/adidas/adidasModel3.webp'
 export { default as nikeModel1 } from '../assets/nike/nikeModel1.jpg'
 export { default as nikeModel2 } from '../assets/nike/nikeModel2.jpg'
 export { default as nikeModel3 } from '../assets/nike/nikeModel3.jpg'
+
+
+// Puma models
+export { default as pumaModel1 } from '../assets/puma/pumaModel1.jpg'
+export { default as pumaModel2 } from '../assets/puma/pumaModel2.jpg'
+export { default as pumaModel3 } from '../assets/puma/pumaModel3.jpg'
